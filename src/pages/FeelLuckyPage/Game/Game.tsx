08379a4ece6212bc87334b1041bad68b919efee8
Léonard Wojcik <leonardw@tutanota.com>
2024-06-08 18:50:49 +0200

    feat: added some confettis!

--- src/pages/FeelLuckyPage/Game/Game.tsx
+++ src/pages/FeelLuckyPage/Game/Game.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "../Card/Card";
 import classes from "./Game.module.css";
 import { shuffle } from "../../../helpers/utils";
 import cards, { ICard } from "../cards";
+import JSConfetti from "js-confetti";
 
 export default function Game() {
     shuffle(cards);
@@ -13,6 +14,27 @@ export default function Game() {
     );
     const [gameCards, setGameCards] = useState(cards);
     const [nextCard, setNextCard] = useState<ICard | null>(null);
+
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const confettiRef = useRef<JSConfetti | null>(null);
+
+    // Init canvas on first render.
+    useEffect(() => {
+        // The JSConfetti library is handling the canvas prop as "HTMLCanvasElement | undefined" instead of "HTMLCanvasElement | undefined" :/
+        const canvas = canvasRef.current ?? undefined;
+        confettiRef.current = new JSConfetti({ canvas });
+    }, []);
+
+    useEffect(() => {
+        if (gameCardsLength === 0 && confettiRef.current) {
+            console.log("You win!");
+            // TODO: scroll to top?
+            confettiRef.current.addConfetti({
+                confettiRadius: 5,
+                confettiNumber: 300,
+            });
+        }
+    }, [gameCardsLength]);
 
     return (
         <>
@@ -30,6 +52,7 @@ export default function Game() {
                     />
                 ))}
             </div>
+            <canvas className={classes.canvas} ref={canvasRef}></canvas>
         </>
     );
 }
