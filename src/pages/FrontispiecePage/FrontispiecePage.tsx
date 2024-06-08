03f9f0b17df93bbfeb1ba8e688371ccddc0dfc9c
Léonard Wojcik <leonardw@tutanota.com>
2024-06-08 11:05:22 +0200

    Just some styling 🎨...

--- src/pages/FrontispiecePage/FrontispiecePage.tsx
+++ src/pages/FrontispiecePage/FrontispiecePage.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import classes from "./FrontispiecePage.module.css";
 
 export default function FrontispiecePage() {
     return (
-        <div>
-            <h1>Florilegium Absurdae</h1>
-            <Link to="/home">Enter</Link>
+        <div className={classes.wrapper}>
+            <div>
+                <h1>Florilegium Absurdae</h1>
+                <Link to="/home">Enter</Link>
+            </div>
         </div>
     );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
+import classes from "./HomePage.module.css";
 
 export default function HomePage() {
     return (
-        <div>
-            <h1>Florilegium Absurdae</h1>
+        <div className={classes.home}>
             <div>
-                <ul>
-                    <li>
-                        <Link to="/feel-lucky">Feel lucky</Link>
-                    </li>
-                </ul>
+                <h1>Florilegium Absurdae</h1>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/feel-lucky">Feel lucky</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
