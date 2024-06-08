868732976214ecb4a86c08134df0d7d5c4c8cf10
Léonard Wojcik <leonardw@tutanota.com>
2024-06-08 14:12:28 +0200

    Added some new pictures :D

--- src/pages/FeelLuckyPage/cards.ts
+++ src/pages/FeelLuckyPage/cards.ts
@@ -1,6 +1,14 @@
-import alkan from "../../assets/feel-lucky/alkan.png";
-import chopin from "../../assets/feel-lucky/chopin.jpg";
-import bach from "../../assets/feel-lucky/bach.jpg";
+import dice from "../../assets/feel-lucky/dice-six.avif";
+import trefle from "../../assets/feel-lucky/trefle.avif";
+import horseshoe from "../../assets/feel-lucky/horseshoe.avif";
+import seven from "../../assets/feel-lucky/seven.svg";
+import amanita from "../../assets/feel-lucky/amanita.avif";
+import ladybird from "../../assets/feel-lucky/ladybird.avif";
+import maneki from "../../assets/feel-lucky/maneki.avif";
+import pig from "../../assets/feel-lucky/pig.avif";
+import acorn from "../../assets/feel-lucky/acorn.avif";
+import bamboo from "../../assets/feel-lucky/bamboo.avif";
+import rainbow from "../../assets/feel-lucky/rainbow.avif";
 
 export interface ICard {
     image: string;
@@ -9,16 +17,48 @@ export interface ICard {
 
 const cards: ICard[] = [
     {
-        image: alkan,
-        value: "Alkan",
+        image: dice,
+        value: "Dice",
     },
     {
-        image: chopin,
-        value: "Chopin",
+        image: trefle,
+        value: "Trefle",
     },
     {
-        image: bach,
-        value: "Bach",
+        image: horseshoe,
+        value: "Horseshoe",
+    },
+    {
+        image: seven,
+        value: "Seven",
+    },
+    {
+        image: amanita,
+        value: "Amanita",
+    },
+    {
+        image: ladybird,
+        value: "LadyBird",
+    },
+    {
+        image: maneki,
+        value: "Maneki",
+    },
+    {
+        image: pig,
+        value: "Pig",
+    },
+    {
+        image: acorn,
+        value: "Acorn",
+    },
+    {
+        image: bamboo,
+        value: "Bamboo",
+    },
+    {
+        image: rainbow,
+        value: "Rainbow",
     },
 ];
 
