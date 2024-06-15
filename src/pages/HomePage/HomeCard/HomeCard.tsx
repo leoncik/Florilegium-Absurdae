c908fd22c67f9cfdf9a7f24740149c0ae6717b5d
Léonard Wojcik <leonardw@tutanota.com>
2024-06-15 15:46:07 +0200

    Polishing the home cards just a bit

--- src/pages/HomePage/HomeCard/HomeCard.tsx
+++ src/pages/HomePage/HomeCard/HomeCard.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import classes from "./HomeCard.module.css";
+import { motion } from "framer-motion";
 
 export interface HomeCardProps {
     url: string;
@@ -9,12 +10,14 @@ export interface HomeCardProps {
 
 export default function HomeCard({ url, image, text }: HomeCardProps) {
     return (
-        <Link to={url}>
-            <div
-                className={classes.wrapper}
-                style={{ backgroundImage: `url("${image}")` }}
-            ></div>
-            <p className={classes.text}>{text}</p>
-        </Link>
+        <motion.div whileHover={{ scale: 1.05 }}>
+            <Link to={url}>
+                <div
+                    className={classes.wrapper}
+                    style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+                <p className={classes.text}>{text}</p>
+            </Link>
+        </motion.div>
     );
 }
