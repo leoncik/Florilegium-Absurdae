b3a8e890cc3810a275b3f5d48ece1c917880b849
Léonard Wojcik <leonardw@tutanota.com>
2024-06-16 15:28:25 +0200

    feat: highlight active options

--- src/pages/OptionsPage/OptionsPage.tsx
+++ src/pages/OptionsPage/OptionsPage.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 import useOptionsStore from "../../stores/optionsStore";
 import { useT } from "talkr";
 import { useAutocompleteT } from "../../hooks/useAutocompleteT";
+import classes from "./Options.module.css";
 
 export default function OptionsPage() {
+    const currentTheme = useOptionsStore((state) => state.theme);
+    const currentLanguage = useOptionsStore((state) => state.language);
     const setTheme = useOptionsStore((state) => state.setTheme);
     const setLanguage = useOptionsStore((state) => state.setLanguage);
     const { T } = useAutocompleteT();
@@ -20,19 +23,34 @@ export default function OptionsPage() {
             <h1>{T("options.title")}</h1>
             <h2>{T("options.themeSelect")}</h2>
             <div>
-                <ul>
+                <ul className={classes.themes}>
                     <li>
-                        <button onClick={() => setTheme("light")}>
+                        <button
+                            onClick={() => setTheme("light")}
+                            className={
+                                currentTheme === "light" ? classes.active : ""
+                            }
+                        >
                             {T("options.light")}
                         </button>
                     </li>
                     <li>
-                        <button onClick={() => setTheme("dark")}>
+                        <button
+                            onClick={() => setTheme("dark")}
+                            className={
+                                currentTheme === "dark" ? classes.active : ""
+                            }
+                        >
                             {T("options.dark")}{" "}
                         </button>
                     </li>
                     <li>
-                        <button onClick={() => setTheme("retro")}>
+                        <button
+                            onClick={() => setTheme("retro")}
+                            className={
+                                currentTheme === "retro" ? classes.active : ""
+                            }
+                        >
                             {T("options.retro")}{" "}
                         </button>
                     </li>
@@ -40,12 +58,26 @@ export default function OptionsPage() {
             </div>
 
             <h2>{T("options.languageSelect")}</h2>
-            <ul>
+            <ul className={classes.languages}>
                 <li>
-                    <button onClick={() => handleSetLanguage("fr")}>FR</button>
+                    <button
+                        className={
+                            currentLanguage === "fr" ? classes.active : ""
+                        }
+                        onClick={() => handleSetLanguage("fr")}
+                    >
+                        FR
+                    </button>
                 </li>
                 <li>
-                    <button onClick={() => handleSetLanguage("en")}>EN</button>
+                    <button
+                        className={
+                            currentLanguage === "en" ? classes.active : ""
+                        }
+                        onClick={() => handleSetLanguage("en")}
+                    >
+                        EN
+                    </button>
                 </li>
             </ul>
         </div>
