ec8cd966908c7720477cb697287ce82d819748bc
Léonard Wojcik <leonardw@tutanota.com>
2024-06-16 11:53:22 +0200

    feat: add translations

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,22 +1,37 @@
 import classes from "./HomePage.module.css";
-import { pages } from "./pages";
-import HomeCard from "./HomeCard/HomeCard";
+import HomeCard, { HomeCardProps } from "./HomeCard/HomeCard";
 import { Link } from "react-router-dom";
 import useOptionsStore from "../../stores/optionsStore";
 import settings from "../../assets/home/settings.svg";
 import { motion } from "framer-motion";
 import { useAutocompleteT } from "../../hooks/useAutocompleteT";
+import back from "../../assets/feel-lucky/card-back.png";
+import todo from "../../assets/home/todo.jpg";
 
 export default function HomePage() {
     const currentTheme = useOptionsStore((state) => state.theme);
     const { T } = useAutocompleteT();
 
+    const pages: HomeCardProps[] = [
+        {
+            url: "/feel-lucky",
+            image: back,
+            text: T("pages.feelLucky"),
+        },
+        {
+            url: "/todo-the-game",
+            image: todo,
+            text: T("pages.todoGame"),
+        },
+    ];
+
     return (
         <div className={classes.home}>
             <div>
                 <h1>Florilegium Absurdae</h1>
-                <p>{T("test")}</p>
-                <p>Current theme: {currentTheme}</p>
+                <p>
+                    {T("home.currentTheme")} {currentTheme}
+                </p>
                 <div className={classes["pages-wrapper"]}>
                     {pages.map((page) => (
                         <HomeCard
